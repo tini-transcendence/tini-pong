7611f416e93638de026827ce8d5e6ff970810976
wbae <rsursu0@naver.com>
2024-03-14 14:23:28 +0900

    :sparkles: feat: admin에서 user 추가 후 방 생성, 넘겨주는 테스트 페이지 완료

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Room
+import uuid
 
-# Register your models here.
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("uuid", "name", "type", "difficulty", "owner_uuid")
+    list_filter = ("type", "difficulty", "owner_uuid")
+    search_fields = (
+        "name",
+        "owner_uuid__username",
+    )  # User 모델의 username 필드를 검색 대상으로 설정
+    readonly_fields = ("uuid",)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs
