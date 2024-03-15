1ea42bdb358e27ae1a7e92042d5242cf8967f3ab
Conut-1 <1mim1@naver.com>
2024-03-15 16:42:05 +0900

    ✨feat: 로그인 시 Access Token, Refresh Token 생성

--- server/user/views.py
+++ server/user/views.py
@@ -1,11 +1,12 @@
 import requests
 import os
 
-from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest
+from django.http import JsonResponse, HttpRequest, HttpResponseBadRequest
 from django.views import View
 from django.urls import reverse
 
 from util.jwt import create
+from util.timestamp import get_timestamp
 
 from .models import User
 
@@ -39,4 +40,16 @@ class LoginOauthView(View):
             id_42=user_42_logged_in,
             defaults={"otp_secret": "otp_secret", "nickname": user_42_logged_in},
         )
-        return HttpResponse(create({"test": "test"}, "secret", 0))
+        access_token = create(
+            {"uuid": str(user_logged_in.uuid)},
+            os.environ.get("ACCESS_SECRET"),
+            get_timestamp(minutes=30),
+        )
+        refresh_token = create(
+            {"uuid": str(user_logged_in.uuid)},
+            os.environ.get("REFRESH_SECRET"),
+            get_timestamp(days=14),
+        )
+        return JsonResponse(
+            {"access_token": access_token, "refresh_token": refresh_token}
+        )
