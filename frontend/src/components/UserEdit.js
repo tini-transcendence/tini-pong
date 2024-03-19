80665519b365954b40fc19a7e2ec4b1eecefac42
d2geut <d2geut@naver.com>
2024-03-19 16:18:45 +0900

    ✨ feat: 이미지 업로드 및 상태메세지 입력 추가

--- frontend/src/components/UserEdit.js
+++ frontend/src/components/UserEdit.js
@@ -11,43 +11,42 @@ export default class extends AbstractComponent {
 
 	async getHtml() {
 		return `
-		<div class="justify-content-center" id="image-preview">
+		<div class="justify-content-center">
+			<img id="image-preview" src="./src/img/default_profile.png" width="200px" height="200px" alt="profile thumbnail" style="border-radius: 20%;"/>
 		</div>
 		<div class="mb-3">
-			<label for="formFile" class="form-label">Profile Image</label>
-			<!-- <input class="form-control" type="file" id="formFile" accept="image/*"> -->
-			<input class="form-control" type="file" id="formFile">
+			<label for="profile-form" class="form-label">Profile Image</label>
+			<input class="form-control" type="file" id="profile-form" accept="image/*">
+		</div>
+		<div class="mb-3">
+			<label for="message-form" class="form-label">Status message</label>
+			<input type="text" class="form-control" id="message-form" placeholder="Enter your status message">
+		</div>
+		<div>
+			<button class="btn btn-outline-primary btn-lg" id="editpage-submit">Submit</button>
 		</div>
 		`;
 	}
 
 	handleRoute() {
-		const uploadImage = document.querySelector("#formFile");
+		const uploadImage = document.querySelector("#profile-form");
 		uploadImage.addEventListener("change", e => {
-			console.log("file upload!");
 			const imgFile = e.target.files[0];
-			console.log(imgFile);
-			console.log(e.target);
 			if (!imgFile.type.match(fileForm)) {
-				alert("이미지 파일만 업로드!");
+				alert("이미지 파일(jpg, jpeg, png, gif)만 업로드!");
 				return ;
 			} else if (imgFile.size > maxFileSize) {
 				alert("2MB 이하 크기만 업로드!");
 				return ;
 			}
 			const imageUrl = URL.createObjectURL(imgFile);
-			document.querySelector("#image-preview").insertAdjacentHTML("beforeend", `
-			<img src="${imageUrl}" width="200px" height="200px" alt="profile thumbnail" style="border-radius: 30%;"/>
-			`);
+			document.querySelector("#image-preview").setAttribute("src", `${imageUrl}`);
+		})
 
-			// const reader = new FileReader();
-			// reader.onload = e => {
-			// 	console.log(e.target.result);
-			// 	document.querySelector("#image-preview").insertAdjacentHTML("beforeend", `
-			// 	<img src="${e.target.result}" width="200px" height="200px"/>
-			// 	`);
-			// }
-			// reader.readAsDataURL(e.target.files[0]);
+		const editPageSubmit = document.querySelector("#editpage-submit");
+		editPageSubmit.addEventListener("click", e => {
+			console.log(document.querySelector("#image-preview").src);
+			console.log(document.querySelector("#message-form").value);
 		})
 	}
 }
