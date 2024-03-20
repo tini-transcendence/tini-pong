049f2a647fb43c9b5951f5b19327e05e2c6845f9
Conut-1 <1mim1@naver.com>
2024-03-20 16:04:43 +0900

    🐛 fix: tupple 분해 할당 수정
    받는 변수 숫자가 맞지 않는 것을 수정

--- server/user/views.py
+++ server/user/views.py
@@ -36,7 +36,7 @@ class LoginOauthView(View):
         if profile_response.status_code != 200:
             return HttpResponseBadRequest()
         user_42_logged_in = profile_response.json().get("login")
-        (user_logged_in,) = User.objects.get_or_create(
+        (user_logged_in, created) = User.objects.get_or_create(
             id_42=user_42_logged_in,
             defaults={"otp_secret": "otp_secret", "nickname": user_42_logged_in},
         )
