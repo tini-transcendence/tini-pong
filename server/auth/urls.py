2cd581ea14e94a579041b07cf373eaf32f453830
Conut-1 <1mim1@naver.com>
2024-03-25 15:44:19 +0900

    🔨 fix: auth url 경로 수정

--- server/auth/urls.py
+++ server/auth/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from .views import RefreshTokenView
 
-urlpatterns = [path("refresh", RefreshTokenView.as_view())]
+urlpatterns = [path("refresh/", RefreshTokenView.as_view())]
