749ad541ec08e7f2b4095e02fd23a9d6176ea0a4
d2geut <d2geut@naver.com>
2024-03-25 15:57:00 +0900

    ✨ feat: LoginModule 추가 (페이지 이동 시 로그인 여부 체크하는 모듈)

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,5 @@
 import AbstractComponent from "./AbstractComponent.js";
+import FetchModule from "../utils/fetchmodule.js"
 
 export default class extends AbstractComponent {
 	constructor() {
@@ -22,7 +23,13 @@ export default class extends AbstractComponent {
 			const refreshToken = localStorage.getItem("refresh_token");
 			if (refreshToken) // refresh token이 존재, accecc token 유무 구분해야함
 			{
-				location.href = "/";
+				try {
+					const fetchModule = new FetchModule();
+					fetchModule.getReIssuedAccessToken(refreshToken);
+					location.href = "/";
+				} catch (error) {
+					location.href = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-a44af88cc4ff0433fc32ca3cc93bc26a96ced1a9b0a7c5293de5fb6418ab5707&redirect_uri=https%3A%2F%2Flocalhost%2Flogin%2Foauth&response_type=code";
+				}
 			}
 			else // refresh token 만료
 				location.href = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-a44af88cc4ff0433fc32ca3cc93bc26a96ced1a9b0a7c5293de5fb6418ab5707&redirect_uri=https%3A%2F%2Flocalhost%2Flogin%2Foauth&response_type=code";
