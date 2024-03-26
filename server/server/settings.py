24045aefc245be50d975d00b86e77fc6ab818b48
wbae <rsursu0@naver.com>
2024-03-26 17:13:16 +0900

    ✨ feat: daphne 추가 및 인메모리 캐시 웹소켓 asgi 설정

--- server/server/settings.py
+++ server/server/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -56,10 +57,7 @@ INSTALLED_APPS = [
 ASGI_APPLICATION = "server.asgi.application"
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
 
