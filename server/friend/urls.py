29201405d72435b341b0a95b49312d6d0ca52253
Conut-1 <1mim1@naver.com>
2024-03-27 16:09:49 +0900

    ✨ feat: 친구 추가 API

--- server/friend/urls.py
+++ server/friend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import FriendListView
+from .views import FriendListView, AddFriendView
 
 urlpatterns = [
     path("", FriendListView.as_view()),
+    path("add/", AddFriendView.as_view()),
 ]
--- server/friend/views.py
+++ server/friend/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpRequest, JsonResponse
+import json
+from http import HTTPStatus
+
+from django.http import HttpRequest, JsonResponse, HttpResponse
 from django.views import View
+from django.db import IntegrityError
 
 from .models import Friend
 
@@ -15,3 +19,13 @@ class FriendListView(View):
                 {"uuid": friend.user_to.uuid, "nickname": friend.user_to.nickname}
             )
         return JsonResponse(friend_list_response, safe=False)
+
+
+class AddFriendView(View):
+    def post(self, request: HttpRequest):
+        try:
+            user_to = json.loads(request.body)["target_uuid"]
+            Friend.objects.create(user_from=request.user_uuid, user_to_id=user_to)
+            return HttpResponse(status=HTTPStatus.CREATED)
+        except IntegrityError:
+            return HttpResponse(status=HTTPStatus.CONFLICT)
