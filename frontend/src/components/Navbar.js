3079a14e7a87a51f200c35b197732ecafb7813a2
d2geut <d2geut@naver.com>
2024-03-28 21:24:08 +0900

    ✨ feat: Friend 레이아웃에 stauts 추가 및 수정중

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -49,13 +49,22 @@ export default class extends AbstractComponent {
 		const newFriendListNode = document.createElement("li");
 		newFriendListNode.setAttribute("class", "list-group-item");
 		newFriendListNode.insertAdjacentHTML("beforeend", `
-		<a href="/users/${element.nickname}" class="link-offset-2 link-underline link-underline-opacity-0 link-dark">${element.nickname}</a>
-		<button class="btn btn-outline-secondary" type="button">
-			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-dash" viewBox="0 0 16 16">
-				<path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M11 12h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1 0-1m0-7a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
-				<path d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z"/>
-			</svg>
-		</button>
+		<div class="d-flex flex-wrap">
+			<div class="p-2 me-auto">
+				<a href="/users/${element.nickname}" class="link-offset-2 link-underline link-underline-opacity-0 link-dark">${element.nickname}</a>
+			</div>
+			<div class="p-2">
+				online
+			</div>
+			<div>
+				<button class="btn btn-outline-secondary" type="button">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-dash" viewBox="0 0 16 16">
+						<path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M11 12h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1 0-1m0-7a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
+						<path d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z"/>
+					</svg>
+				</button>
+			</div>
+		</div>
 		`);
 		document.querySelector("#friend-list").appendChild(newFriendListNode);
 
