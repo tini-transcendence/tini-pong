346a86fc970d7a753b14faaa1d3efc660bb1340a
d2geut <d2geut@naver.com>
2024-04-01 18:35:24 +0900

    ✨ feat: OTP 인증 페이지 추가

---
+++ frontend/src/components/Otp.js
@@ -0,0 +1,103 @@
+import AbstractComponent from "./AbstractComponent.js";
+import FetchModule from "../utils/fetchmodule.js";
+
+const regex = /^[0-9]+$/;
+const regex2 = /^[0-9]$/;
+
+export default class extends AbstractComponent {
+	constructor() {
+		super();
+		this.setTitle("OTP Auth");
+	}
+
+	async getHtml() {
+		return `
+		<div class="container-sm text-center">
+			<p><b>Verification Code</b></p>
+			<p>Google Authenticator에 표시된 OTP 코드를 입력해주세요.</p>
+			<div class="row row-cols-3 row-cols-sm-6">
+				<div class="col">
+					<input class="form-control form-control-lg otp-form" type="number" style="text-align: center" autofocus>
+				</div>
+				<div class="col">
+					<input class="form-control form-control-lg otp-form" type="number" style="text-align: center">
+				</div>
+				<div class="col">
+					<input class="form-control form-control-lg otp-form" type="number" style="text-align: center">
+				</div>
+				<div class="col">
+					<input class="form-control form-control-lg otp-form" type="number" style="text-align: center">
+				</div>
+				<div class="col">
+					<input class="form-control form-control-lg otp-form" type="number" style="text-align: center">
+				</div>
+				<div class="col">
+					<input class="form-control form-control-lg otp-form" type="number" style="text-align: center">
+				</div>
+			</div>
+			<div class="mt-2">
+				<p class="text-danger" id="otp-danger">&nbsp</p>
+				<button class="btn btn-primary" id="otp-submit-btn">제출</button>
+			</div>
+		</div>
+		`;
+	}
+
+	handleRoute() {
+		const otpForm = document.querySelectorAll(".otp-form");
+		const otpSubmitBtn = document.querySelector("#otp-submit-btn");
+		const otpDanger = document.querySelector("#otp-danger");
+
+		[].forEach.call(otpForm, (form, i) => {
+			form.addEventListener("input", (event) => {
+				if (!regex.test(event.target.value))
+					event.target.value = null;
+				else {
+					event.target.value = event.target.value.slice(-1);
+				}
+				if (i === 5)
+					otpSubmitBtn.focus();
+				else
+					otpForm[i+1].focus();
+			})
+		})
+
+		otpSubmitBtn.addEventListener("click", async event => {
+			otpDanger.innerText = "\u00A0";
+			let otpNum = '';
+			try {
+				[].forEach.call(otpForm, (form) => {
+					const formval = form.value;
+					if (!regex2.test(formval))
+						throw new Error("OTP 코드를 모두 입력해주세요.");
+					else {
+						otpNum = otpNum + formval;
+					}
+				})
+
+				const fetchModule = new FetchModule();
+				const response = await fetchModule.request(new Request("http://localhost:8000/auth/otp/", {
+					method: 'POST',
+					credentials: "include",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						otp_code: otpNum,
+					}),
+				}));
+				if (response.ok) {
+					const data = await response.json();
+					localStorage.setItem("refresh_token", data.refresh_token);
+					location.href = "/";
+				}
+				else if (response.status === 401)
+					throw new Error("유효하지 않는 요청입니다.");
+				else if (response.status === 400)
+					throw new Error("OTP 코드가 유효하지 않습니다.");
+			} catch (error) {
+				otpDanger.innerText = error.message;
+			}
+		})
+	}
+}
--- frontend/src/components/OtpQR.js
+++ frontend/src/components/OtpQR.js
@@ -15,9 +15,10 @@ export default class extends AbstractComponent {
 			<li>Play 스토어에서 <b>Google Authenticator</b>를 다운로드합니다.</li>
 			<li>앱에서 <b>+ 버튼</b>을 누르고 <b>QR 코드 스캔</b>을 선택합니다.</li>
 			<li>아래의 QR 코드를 스캔합니다.</li>
-			<li>등록이 완료되었으면 다음 버튼을 통해 인증 페이지로 넘어갑니다.</li>
+			<li>등록이 완료되었으면 확인 버튼을 통해 인증 페이지로 넘어갑니다.</li>
 			</ul>
 		<div id="qrcode"></div>
+		<button class="btn btn-primary" data-href="/login/otp">확인</button>
 		`;
 	}
 
