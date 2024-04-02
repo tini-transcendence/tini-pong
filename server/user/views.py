8bd05ff37567558ef3c9410398104d98bb536c69
Conut-1 <1mim1@naver.com>
2024-04-02 18:28:12 +0900

    💄 style: 포맷에 맞게 코드 수정

--- server/user/views.py
+++ server/user/views.py
@@ -106,6 +106,10 @@ class LoginOauthView(View):
         )
         response = JsonResponse({"refresh_token": refresh_token})
         response.set_cookie(
-            key="access_token", value=access_token, httponly=True, secure=True, samesite="None"
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,
+            samesite="None",
         )
         return response
