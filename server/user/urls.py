6ec10892717540c52b2e8069ed97f7a5381da4ae
Conut-1 <1mim1@naver.com>
2024-04-05 16:24:12 +0900

    🔨 refactore: 불필요한 코드 제거

--- server/user/urls.py
+++ server/user/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import UserProfileView, EditUserView, LoginOauthView, StatusUpdateView
+from .views import UserProfileView, EditUserView, StatusUpdateView
 
 urlpatterns = [
     path("profile/", UserProfileView.as_view()),
     path("edit/", EditUserView.as_view()),
     path("status-update/", StatusUpdateView.as_view()),
-    path("login/oauth/", LoginOauthView.as_view(), name="oauth"),
 ]
