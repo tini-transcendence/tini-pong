28f58558177beae18db2a07140d2ec80f882b06d
wbae <rsursu0@naver.com>
2024-04-08 19:22:35 +0900

    🐛 fix: 시리얼라이저 모델에 맞게 수정

--- server/dashboard/serializers.py
+++ server/dashboard/serializers.py
@@ -18,10 +18,24 @@ class OneVsOneGameResultSerializer(serializers.ModelSerializer):
 class TwoVsTwoGameResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = TwoVsTwoGameResult
-        fields = ["win", "start_time", "score_team1", "score_team2"]
+        fields = [
+            "win",
+            "start_time",
+            "team1_player1",
+            "team1_player2",
+            "team2_player1",
+            "team2_player2",
+            "score_team1",
+            "score_team2",
+        ]
 
 
 class TournamentGameResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = TournamentGameResult
-        fields = ["win", "start_time", "final_scores"]
+        fields = [
+            "win",
+            "start_time",
+            "players",
+            "final_scores",
+        ]
