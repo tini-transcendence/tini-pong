ce57cddd92ec835fffb229028411f3ee884d7a76
wbae <rsursu0@naver.com>
2024-04-08 17:48:40 +0900

    ✨ feat: 게임 타입별 데이터 저장 모델 작성

--- server/dashboard/models.py
+++ server/dashboard/models.py
@@ -1,46 +1,57 @@
 import uuid
 from django.db import models
 from user.models import User
+from room.models import Room
 
 
 # Create your models here.
+class GameResult(models.Model):
+    room = models.ForeignKey(
+        Room, on_delete=models.CASCADE, related_name="game_results"
+    )
+    start_time = models.DateTimeField()
+    win = models.BooleanField()
+
+    class Meta:
+        abstract = True
 
 
-class TournamentHistory(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    play_time = models.DateTimeField()
-    user1 = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="tournament_histories_user1"
+class OneVsOneGameResult(GameResult):
+    player1 = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="one_v_one_results_as_player1"
     )
-    user2 = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="tournament_histories_user2"
+    player2 = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="one_v_one_results_as_player2"
     )
-    user3 = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="tournament_histories_user3"
-    )
-    user4 = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="tournament_histories_user4"
-    )
-    win_user = models.CharField(max_length=255)
+    score_player1 = models.IntegerField()
+    score_player2 = models.IntegerField()
 
 
-class WinLoseHistory(models.Model):
-    auto_increment = models.AutoField(primary_key=True)
-    user_uuid = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="win_lose_histories"
+class TwoVsTwoGameResult(GameResult):
+    team1_player1 = models.ForeignKey(
+        User,
+        related_name="two_v_two_results_as_team1_player1",
+        on_delete=models.CASCADE,
     )
-    game_count = models.IntegerField(default=0)
-    victory_count = models.IntegerField(default=0)
-    defeat_count = models.IntegerField(default=0)
+    team1_player2 = models.ForeignKey(
+        User,
+        related_name="two_v_two_results_as_team1_player2",
+        on_delete=models.CASCADE,
+    )
+    team2_player3 = models.ForeignKey(
+        User,
+        related_name="two_v_two_results_as_team2_player1",
+        on_delete=models.CASCADE,
+    )
+    team2_player4 = models.ForeignKey(
+        User,
+        related_name="two_v_two_results_as_team2_player2",
+        on_delete=models.CASCADE,
+    )
+    score_team1 = models.IntegerField()
+    score_team2 = models.IntegerField()
 
 
-class OneVsOneHistory(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    play_time = models.DateTimeField()
-    user1 = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="one_vs_one_histories_user1"
-    )
-    user2 = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="one_vs_one_histories_user2"
-    )
-    win_user = models.CharField(max_length=255)
+class TournamentGameResult(GameResult):
+    players = models.ManyToManyField(User, related_name="tournament_results")
+    final_scores = models.JSONField()
