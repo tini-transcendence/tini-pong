8c5a5fdab752826af0c702bf3b33f169839c885a
d2geut <d2geut@naver.com>
2024-04-10 19:21:09 +0900

    🐛 fix: 이미지 상대경로 수정, Mixed Content 문제 해결

--- frontend/src/components/UserEdit.js
+++ frontend/src/components/UserEdit.js
@@ -26,7 +26,7 @@ export default class extends AbstractComponent {
 			<div class="mb-3">
 				<label for="avatar-form" class="form-label">Avatar Image</label>
 				<div class="text-center mb-2">
-					<img id="image-preview" src="./src/img/default_profile.png" width="200px" height="200px" alt="avatar thumbnail" style="border-radius: 20%;"/>
+					<img id="image-preview" src="/src/img/default_profile.png" width="200px" height="200px" alt="avatar thumbnail" style="border-radius: 20%;"/>
 				</div>
 				<input class="form-control" type="file" id="avatar-form" accept="image/*">
 			</div>
