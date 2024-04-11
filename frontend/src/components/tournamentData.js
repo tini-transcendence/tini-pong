9e457c1ef6eb265524f9046fa97b275d5d5ed895
Youngjin Park <youngjpa@c4r8s1.42seoul.kr>
2024-04-11 19:26:02 +0900

    fix: 변수 선언 방식 변경

--- frontend/src/components/tournamentData.js
+++ frontend/src/components/tournamentData.js
@@ -37,30 +37,30 @@ export default class extends AbstractComponent {
     populateFunction(data) {
         console.log(typeof(data));
 
-        var tbody = document.querySelector('.b');
+        let tbody = document.querySelector('.b');
 
         data.tournamentLog.forEach(function(tournamentData) {
-            var ul = document.createElement('ul');
+            let ul = document.createElement('ul');
             ul.classList.add('tdataBox', 'list-group', 'd-flex', 'justify-content-between');
 
-            var div = document.createElement('div');
+            let div = document.createElement('div');
             div.classList.add('row');
             
-            var col3 = document.createElement('div');
+            let col3 = document.createElement('div');
             col3.classList.add('col-3');
             
-            var timestampDiv = document.createElement('div');
+            let timestampDiv = document.createElement('div');
             timestampDiv.classList.add('align-items-center', 'timestemp', 'noto-sans');
-            var timestamp = new Date(tournamentData.tournament[0].timestamp * 1000);
-            var timestampOptions = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
-            var timestampString = new Intl.DateTimeFormat('en-US', timestampOptions).format(timestamp);
+            let timestamp = new Date(tournamentData.tournament[0].timestamp * 1000);
+            let timestampOptions = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+            let timestampString = new Intl.DateTimeFormat('en-US', timestampOptions).format(timestamp);
             timestampString = timestampString.replace(',', '<br>');
             timestampDiv.innerHTML = timestampString;
             col3.appendChild(timestampDiv);
 
             div.appendChild(col3);
             
-            var col9 = document.createElement('div');
+            let col9 = document.createElement('div');
             col9.classList.add('col-9');
             
             
@@ -68,22 +68,22 @@ export default class extends AbstractComponent {
                 
                 if (matchData.hasOwnProperty('timestamp')) {
                 } else {
-                    var game = document.createElement('div');
+                    let game = document.createElement('div');
                     game.classList.add('gameLine', 'justify-content-between', 'align-items-center', 'my-2', 'row');
 
-                    var rankSpan = document.createElement('span');
+                    let rankSpan = document.createElement('span');
                     rankSpan.classList.add('col-3', 'textAlign', 'game-rank');
                     rankSpan.textContent = matchData.index === 1 ? '1st' : matchData.index === 2 ? '2nd' : matchData.index === 3 ? 'Final' : matchData.index + 'th';
                     
-                    var playerASpan = document.createElement('span');
+                    let playerASpan = document.createElement('span');
                     playerASpan.classList.add('col', 'textAlign', 'game-player');
                     playerASpan.textContent = matchData.playerA.name;
                     
-                    var scoreSpan = document.createElement('span');
+                    let scoreSpan = document.createElement('span');
                     scoreSpan.classList.add('col-3', 'textAlign', 'game-score');
                     scoreSpan.textContent = matchData.playerA.score + ' : ' + matchData.playerB.score;
                     
-                    var playerBSpan = document.createElement('span');
+                    let playerBSpan = document.createElement('span');
                     playerBSpan.classList.add('col', 'textAlign', 'game-opponent');
                     playerBSpan.textContent = matchData.playerB.name;
                     
