88e5e92b486ba58a4656bc37e3a7a84d5f5aa40e
Youngjin Park <youngjpa@c4r8s1.42seoul.kr>
2024-04-11 19:06:14 +0900

    fix: 토너먼트 블록체인 데이터 디자인 변경

--- frontend/src/components/tournamentData.js
+++ frontend/src/components/tournamentData.js
@@ -4,6 +4,7 @@ import AbstractComponent from "./AbstractComponent.js";
 import FetchModule from "../utils/fetchmodule.js";
 import {BACKEND_URL} from "../index.js";
 
+
 export default class extends AbstractComponent {
     constructor() {
         super();
@@ -12,44 +13,19 @@ export default class extends AbstractComponent {
 
     async getHtml() {
         return `
-        <style>
-        .b {
-            /* border: solid 1px black; */
-            max-width: 750px;
-            overflow: auto;
-            margin: 0 auto;
-            margin-bottom: 30px;
-        }
-        
-        .title {
-            text-align: center;
-            color: brown;
-            margin-bottom: 10px;
-        }
-        </style>
-        
+
         <body>
         
         <div class="container">
-        <h1 class="display-7 title">Pong Tournament Data from Blockchain.</h1>
-        <table class="table b">
-        <thead>
-        <tr>
-        <th scope="col">Time Stemp</th>
-        <th scope="col">Game 1</th>
-        <th scope="col">Game 2</th>
-        <th scope="col">Final</th>
-        </tr>
-        </thead>
-        <tbody id="tournamentData">
-        <!-- JavaScript로 생성될 내용이 여기에 들어갈 것입니다. -->
-        </tbody>
-        </table>
+            <div class="b">
+            <!-- 토너먼트 데이터가 입력되는 곳 -->
+            </div>
+    
         </div>
         
         <div class="container">
         <div class="text-end" id="btn">
-        <button type="button" class="btn btn-primary" id="goBackButton">돌아가기</button>
+        <button type="button" class="btn btn-outline-dark" id="goBackButton">돌아가기</button>
         </div>
         <script src="tournamentData.js"></script>
         </div>
@@ -57,40 +33,73 @@ export default class extends AbstractComponent {
         </body>
 		`;
     }
-    // 토너먼트 데이터를 표에 채우는 함수
-    populateTable(data) {
-        let tbody = document.getElementById("tournamentData");
-        data.tournamentLog.forEach(function (tournament) {
-            let tr = document.createElement("tr");
-            // Timestamp 추가
-            let tdTimestamp = document.createElement("td");
-            let timestamp = new Date(tournament.tournament[0].timestamp * 1000);
-            let options = {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit'
-            };
-            let formattedTimestamp = timestamp.toLocaleDateString('en-GB', options).replace(/\//g, '.').replace(",", "");
-            tdTimestamp.textContent = formattedTimestamp;
-            tr.appendChild(tdTimestamp);
 
-            // 플레이어 데이터 추가
-            for (let i = 1; i < tournament.tournament.length; i++) {
-                let tdPlayer = document.createElement("td");
-                let playerA = tournament.tournament[i].playerA.name + " " + tournament.tournament[i].playerA.score + " " + (tournament.tournament[i].playerA.score > tournament.tournament[i].playerB.score ? "W" : "L");
-                let playerB = tournament.tournament[i].playerB.name + " " + tournament.tournament[i].playerB.score + " " + (tournament.tournament[i].playerB.score > tournament.tournament[i].playerA.score ? "W" : "L");
-                tdPlayer.innerHTML = `${playerA}<br>${playerB}`;
-                tr.appendChild(tdPlayer);
-            }
+    populateFunction(data) {
+        console.log(typeof(data));
 
-            // 행 추가
-            tbody.appendChild(tr);
+        var tbody = document.querySelector('.b');
+
+        data.tournamentLog.forEach(function(tournamentData) {
+            var ul = document.createElement('ul');
+            ul.classList.add('tdataBox', 'list-group', 'd-flex', 'justify-content-between');
+
+            var div = document.createElement('div');
+            div.classList.add('row');
+            
+            var col3 = document.createElement('div');
+            col3.classList.add('col-3');
+            
+            var timestampDiv = document.createElement('div');
+            timestampDiv.classList.add('align-items-center', 'timestemp', 'noto-sans');
+            var timestamp = new Date(tournamentData.tournament[0].timestamp * 1000);
+            var timestampOptions = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+            var timestampString = new Intl.DateTimeFormat('en-US', timestampOptions).format(timestamp);
+            timestampString = timestampString.replace(',', '<br>');
+            timestampDiv.innerHTML = timestampString;
+            col3.appendChild(timestampDiv);
+
+            div.appendChild(col3);
+            
+            var col9 = document.createElement('div');
+            col9.classList.add('col-9');
+            
+            
+            tournamentData.tournament.forEach(function(matchData) {
+                
+                if (matchData.hasOwnProperty('timestamp')) {
+                } else {
+                    var game = document.createElement('div');
+                    game.classList.add('game', 'justify-content-between', 'align-items-center', 'my-2', 'row');
+
+                    var rankSpan = document.createElement('span');
+                    rankSpan.classList.add('col-3', 'textAlign', 'game-rank');
+                    rankSpan.textContent = matchData.index === 1 ? '1st' : matchData.index === 2 ? '2nd' : matchData.index === 3 ? 'Final' : matchData.index + 'th';
+                    
+                    var playerASpan = document.createElement('span');
+                    playerASpan.classList.add('col', 'textAlign', 'game-player');
+                    playerASpan.textContent = matchData.playerA.name;
+                    
+                    var scoreSpan = document.createElement('span');
+                    scoreSpan.classList.add('col-3', 'textAlign', 'game-score');
+                    scoreSpan.textContent = matchData.playerA.score + ' : ' + matchData.playerB.score;
+                    
+                    var playerBSpan = document.createElement('span');
+                    playerBSpan.classList.add('col', 'textAlign', 'game-opponent');
+                    playerBSpan.textContent = matchData.playerB.name;
+                    
+                    game.appendChild(rankSpan);
+                    game.appendChild(playerASpan);
+                    game.appendChild(scoreSpan);
+                    game.appendChild(playerBSpan);
+                    col9.appendChild(game);
+                }
+                ul.appendChild(div);
+            });
+            div.appendChild(col9);
+            tbody.appendChild(ul);
         });
     }
 
-    // 페이지가 로드될 때 테이블을 채우도록 호출
     goBack() {
         window.history.back();
     }
@@ -102,29 +111,20 @@ export default class extends AbstractComponent {
         (async function (callback) {
             try {
                 const fetchModule = new FetchModule();
-                const response = await fetchModule.request(new Request(`${BACKEND_URL}/dashboard${queryString}`, {
+                const response = await fetchModule.request(new Request(`${BACKEND_URL}/dashboard/tournament/`, {
                     method: 'GET',
                     credentials: "include"
                 }));
                 if (response.ok) {
                     const data = await response.json();
                     callback(data);
-                    // this.populateTable(data);
                 }
                 else
                     throw new Error(response.statusText);
             } catch (error) {
                 console.log(error.message);
             }
-        })(this.populateTable);
-
-        // fetch("http://localhost:8000/dashboard")
-        //     .then((response) => {
-        //         return response.json();
-        //     })
-        //     .then((data) => {
-        //         this.populateTable(data);
-        //     });
+        })(this.populateFunction);
 
         const goBackBtn = document.querySelector("#goBackButton");
         goBackBtn.addEventListener("click", event => {
