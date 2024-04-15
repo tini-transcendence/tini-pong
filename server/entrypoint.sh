362a78a833c3443c2901a6546d314b58bfc63e11
Conut-1 <1mim1@naver.com>
2024-04-15 13:20:42 +0900

    ✨ feat: 로그를 파일로 남기도록 변경

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+mkdir -p ./var/log/celery
+
 python manage.py wait_for_db
 python manage.py migrate
 
-celery -A server worker -l info & \
-celery -A server beat -l info & \
+celery -A server worker -l info --logfile ./var/log/celery/worker.log & \
+celery -A server beat -l info --logfile ./var/log/celery/beat.log & \
 python manage.py runserver 0.0.0.0:9000
